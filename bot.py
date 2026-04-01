cff39f37642f7b8e075cc4a0909fdc69b802a00b
semerehiwet <semerehiwet5@gmail.com>
2026-04-01 09:20:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8601948108:AAGeRbLQ0II9mDdK9p7qmqOM_AOk1FFD_gI")
 
 menu_keyboard = [
     ["የምግብ ፕሮግራም"],
